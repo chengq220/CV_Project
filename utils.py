4339f314fc1dbdfd27651fa3daca7c507592825a
chengq220 <chengqiu220@gmail.com>
2025-11-14 17:12:39 -0500

    got model working

---
+++ utils.py
@@ -0,0 +1,23 @@
+import torch
+import numpy as np
+from PIL import Image
+
+def save_img_tensors_as_grid(img_tensors, nrows, f):
+    img_tensors = img_tensors.permute(0, 2, 3, 1)
+    imgs_array = img_tensors.detach().cpu().numpy()
+    imgs_array[imgs_array < -0.5] = -0.5
+    imgs_array[imgs_array > 0.5] = 0.5
+    imgs_array = 255 * (imgs_array + 0.5)
+    (batch_size, img_size) = img_tensors.shape[:2]
+    ncols = batch_size // nrows
+    img_arr = np.zeros((nrows * batch_size, ncols * batch_size, 3))
+    for idx in range(batch_size):
+        row_idx = idx // ncols
+        col_idx = idx % ncols
+        row_start = row_idx * img_size
+        row_end = row_start + img_size
+        col_start = col_idx * img_size
+        col_end = col_start + img_size
+        img_arr[row_start:row_end, col_start:col_end] = imgs_array[idx]
+
+    Image.fromarray(img_arr.astype(np.uint8), "RGB").save(f"{f}.jpg")
